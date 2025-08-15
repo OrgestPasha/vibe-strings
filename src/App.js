b0b2d5455373df90b74f102a66749be2105e7150
OrgestPasha <orgestpasha2005@gmail.com>
2025-08-15 19:07:06 +0200

    Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
+import React from "react";
 import HomeComponent from "./Home/homeComponent";
-
-
+import FooterComponent from "./Footer/Footer";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <HomeComponent />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomeComponent />} />
+      </Routes>
+      <FooterComponent />
+    </Router>
   );
 }
 
--- src/Home/homeComponent.js
+++ src/Home/homeComponent.js
@@ -11,11 +11,10 @@ const GET_BRANDS = gql`
 `;
 
 function HomeComponent() {
+  const {loading, error, data} = useQuery(GET_BRANDS);
 
-    const {loading, error, data} = useQuery(GET_BRANDS);
-  
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
   return (
     <div class="everything">
       <div class="headerContainer">
@@ -41,7 +40,9 @@ function HomeComponent() {
       </div>
 
       <div class="brand-container">
-        <h2>Featuring the Best <span class='colorText'>Best Brands</span></h2>
+        <h2>
+          Featuring the Best <span class="colorText">Best Brands</span>
+        </h2>
         <p>Select your preferred brand and explore our exquisite collection.</p>
         <div class="brandList">
           {data.findAllBrands.map((brand) => (
@@ -55,42 +56,50 @@ function HomeComponent() {
         </div>
       </div>
 
-      <div class='features-container'>
-          <div class='features-header'>
-            <h2>Why try <span class='colorText'>VibeStrings?</span></h2>
-            <div class='features'>
-              <div class='feature'>
-                <img src="././f1.png" alt="Feature 1" />
-                <h3>Smooth Browsing</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
+      <div class="features-container">
+        <div class="features-header">
+          <h2>
+            Why try <span class="colorText">VibeStrings?</span>
+          </h2>
+          <div class="features">
+            <div class="feature">
+              <img src="././f1.png" alt="Feature 1" />
+              <h3>Smooth Browsing</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
 
-              <div class='feature'>
-                <img src="././f2.png" alt="Feature 1" />
-                <h3>EASY Delivery </h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
+            <div class="feature">
+              <img src="././f2.png" alt="Feature 1" />
+              <h3>EASY Delivery </h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
 
-               <div class='feature'>
-                <img src="././f3.png" alt="Feature 1" />
-                <h3>SwiFT PAYMENTS</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-              
+            <div class="feature">
+              <img src="././f3.png" alt="Feature 1" />
+              <h3>SwiFT PAYMENTS</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
+        </div>
       </div>
 
-      <div class='apps-container'>
-        <div class='app-left'>
-          <h1>Browse and buy your <span class='colorText'>favorite guitars </span>with VibeStrings. </h1>
-          <div class='app-images'>
-             <img src="././appStore.png" alt="App Image" class='app-image'></img>
-          <img src="././playStore.png" alt="App Image" class='app-image'></img>
+      <div class="apps-container">
+        <div class="app-left">
+          <h1>
+            Browse and buy your <span class="colorText">favorite guitars </span>
+            with VibeStrings.{" "}
+          </h1>
+          <div class="app-images">
+            <img src="././appStore.png" alt="App Image" class="app-image"></img>
+            <img
+              src="././playStore.png"
+              alt="App Image"
+              class="app-image"
+            ></img>
           </div>
-          </div>
-        <div class='app-right'>
-          <img src="././appRight.png" alt="App Image" class='app-image'></img>
+        </div>
+        <div class="app-right">
+          <img src="././appRight.png" alt="App Image" class="app-image"></img>
         </div>
       </div>
     </div>
